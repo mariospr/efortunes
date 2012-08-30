00642eb2d5ce3794f9696ecef379c094c6fb420b
Mario Sanchez Prada <mario@mariospr.org>
2012-08-30 23:46:28 +0200

    Update email address

--- src/efortunes_action_login.erl
+++ src/efortunes_action_login.erl
@@ -1,6 +1,6 @@
 %%----------------------------------------------------------------------
 %%
-%% Copyright (C) 2006 Mario Sánchez Prada (msanchez@igalia.com)
+%% Copyright (C) 2006 Mario Sánchez Prada (mario@mariospr.org)
 %%
 %% This program is free software; you can redistribute it and/or
 %% modify it under the terms of version 2 of the GNU General Public
@@ -16,11 +16,11 @@
 %% Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 %% Boston, MA 02111-1307, USA.
 %%
-%% Author: Mario Sánchez Prada (msanchez@igalia.com)
+%% Author: Mario Sánchez Prada (mario@mariospr.org)
 %%----------------------------------------------------------------------
 
-%% @copyright 2006 Mario Sánchez Prada (msanchez@igalia.com)
-%% @author Mario Sánchez Prada (msanchez@igalia.com)
+%% @copyright 2006 Mario Sánchez Prada (mario@mariospr.org)
+%% @author Mario Sánchez Prada (mario@mariospr.org)
 %% @version 0.1 beta, {@date} {@time}.
 %% @end
 
